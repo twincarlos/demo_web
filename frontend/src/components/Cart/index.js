558f1d02cb9460322f249fa28dfbebc6959cc855
twincarlos <twincarlos98@gmail.com>
2023-04-20 16:07:05 -0400

    styles added

--- frontend/src/components/Cart/index.js
+++ frontend/src/components/Cart/index.js
@@ -1,40 +1,45 @@
-import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import * as cartActions from '../../store/thunks/cart';
 import * as orderActions from '../../store/thunks/order';
+import './Cart.css';
 
 function Cart() {
     const dispatch = useDispatch();
     const cart = useSelector(state => state.session.cart);
-    const [itemQuantity, setItemQuantity] = useState(1);
 
     if (!cart) return null;
 
+    const cartItemsArray = Object.values(cart.cartItems);
+
     return (
-        <div className='main' style={{ display: 'flex' }}>
-            <input type='number' onChange={e => setItemQuantity(e.target.value)}></input>
-            {
-                Object.values(cart.cartItems).map(cartItem => (
-                    <div style={{ border: '1px solid black', padding: '10px' }} key={cartItem.id}>
-                        <p>{cartItem.name}</p>
-                        <p>{cartItem.price}</p>
-                        <p>{cartItem.quantity}</p>
-                        <button onClick={() => {
-                            dispatch(cartActions.putOneCartItem({ cartId: cart.cartDetails.id, itemId: cartItem.id, quantity: Number(itemQuantity) }));
-                        }}>Update</button>
-                        <button onClick={() => {
-                            dispatch(cartActions.deleteOneCartItem({ cartId: cart.cartDetails.id, itemId: cartItem.id }))
-                        }}>Remove</button>
-                    </div>
-                ))
-            }
-            <button onClick={() => {
-                dispatch(cartActions.deleteAllCartItems(cart.cartDetails.id))
-            }}>Clear</button>
-            <button onClick={() => {
-                dispatch(orderActions.checkout(cart.cartDetails.id))
-                    .then(stripeCheckout => window.location = stripeCheckout.url);
-            }}>Check out</button>
+        <div className='main cart'>
+            <div id='cart-items'>
+                {
+                    cartItemsArray.map(cartItem => (
+                        <div className='cart-item' key={cartItem.id}>
+                            <img src={cartItem.image} alt=''/>
+                            <div>
+                                <p>{cartItem.name}</p>
+                                <p>${cartItem.price}</p>
+                                <p>Qty: {cartItem.quantity}</p>
+                                <button onClick={() => {
+                                    dispatch(cartActions.deleteOneCartItem({ cartId: cart.cartDetails.id, itemId: cartItem.id }))
+                                }}>Remove</button>
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+            <div id='cart-details'>
+                <p>Subtotal ({cartItemsArray.length} items): <b>${cartItemsArray.reduce((acc, curr) => acc + (curr.price * curr.quantity), 0).toFixed(2)}</b></p>
+                <button onClick={() => {
+                    dispatch(cartActions.deleteAllCartItems(cart.cartDetails.id))
+                }}>Clear</button>
+                <button onClick={() => {
+                    dispatch(orderActions.checkout(cart.cartDetails.id))
+                        .then(stripeCheckout => window.location = stripeCheckout.url);
+                }}>Check out</button>
+            </div>
         </div>
     );
 };
